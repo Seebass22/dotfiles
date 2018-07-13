42b200fad4cbcc962d3e1f84cda6becf10b9de9a
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-07-14 00:42:35 +0200

    polybar weather script now caches

--- polybar/.config/polybar/userscripts/weather.sh
+++ polybar/.config/polybar/userscripts/weather.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 city='Berlin'
 
-curl -s wttr.in/$city > /tmp/weatherfile
-echo "$city: $(cat /tmp/weatherfile | grep ° | head -n 1 | awk '{print $(NF - 1) $NF}' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")"
+update=0
+if [ "$(( $(date +"%s") - $(stat -c "%Y" /tmp/weatherfile) ))" -gt "7200" ]; then
+	update=1
+fi
+
+if [ ! -f /tmp/weatherfile ] || [ $update -eq 1 ] ; then
+	curl -s wttr.in/$city > /tmp/weatherfile
+fi
+
+cat /tmp/weatherfile | grep ° > /dev/null
+if [ $? -eq 0 ] ; then
+	echo "$city: $(cat /tmp/weatherfile | grep ° | head -n 1 | awk '{print $(NF - 1) $NF}' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")"
+else
+	echo ""
+fi
