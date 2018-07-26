cb9d079046b641a4d2a7f1c893a130b65b618442
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-07-26 22:52:53 +0200

    fixed weather script

--- polybar/.config/polybar/userscripts/weather.sh
+++ polybar/.config/polybar/userscripts/weather.sh
@@ -2,16 +2,18 @@
 city='Berlin'
 
 update=0
-if [ "$(( $(date +"%s") - $(stat -c "%Y" /tmp/weatherfile) ))" -gt "7200" ]; then
+if [ ! -f /tmp/weatherfile ] || [ "$(( $(date +"%s") - $(stat -c "%Y" /tmp/weatherfile) ))" -gt "7200" ]
+then
 	update=1
 fi
 
-if [ ! -f /tmp/weatherfile ] || [ $update -eq 1 ] ; then
+if [ $update -eq 1 ] ; then
 	curl -s wttr.in/$city > /tmp/weatherfile
 fi
 
 cat /tmp/weatherfile | grep ° > /dev/null
-if [ $? -eq 0 ] ; then
+if [ $? -eq 0 ]
+then
 	echo "$city: $(cat /tmp/weatherfile | grep ° | head -n 1 | awk '{print $(NF - 1) $NF}' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")"
 else
 	echo ""
