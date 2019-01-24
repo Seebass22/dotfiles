0ca27c3cf066fddf6976ea8f1b6c7296917da755
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-01-24 19:01:31 +0100

    added spotify polybar script

---
+++ polybar/.config/polybar/userscripts/spotify.sh
@@ -0,0 +1,8 @@
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	song="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2)"
+	artist="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2)"
+	echo "$artist - $song"
+else
+	echo ""
+fi
