16d39fe10f65267295fd86d006f224b8d79d62b8
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-01-24 22:28:52 +0100

    spotify script no longer prints errors

--- polybar/.config/polybar/userscripts/spotify.sh
+++ polybar/.config/polybar/userscripts/spotify.sh
@@ -1,7 +1,7 @@
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-	song="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2)"
-	artist="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2)"
+	song="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata 2> /dev/null | sed -n '/title/{n;p}' | cut -d '"' -f 2)"
+	artist="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata 2> /dev/null | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2)"
 	echo "$artist - $song"
 else
 	echo ""
