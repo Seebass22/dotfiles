211562c08ecb846bbf556e8134cb349bdaff2c49
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-08-21 17:47:36 +0200

    polybar: fix multimonitor workspaces

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,7 +3,7 @@
 polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+MONITOR=DisplayPort-2 polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+MONITOR=HDMI-A-0 polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
