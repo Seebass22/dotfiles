c839f04a978fdbc870ed4e3c9d08a5d21e9b7412
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-08-21 17:31:20 +0200

    polybar: rename bars

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
-killall -q polybar
+polybar-msg cmd quit
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar1 and bar2
-MONITOR=DVI-D-1 polybar example &
-MONITOR=HDMI-4 polybar right &
-
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
